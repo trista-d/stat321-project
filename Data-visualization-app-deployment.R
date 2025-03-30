5f557da3a7984489440cdd800cc792a984ace67e
keni-17 <kenickel03@gmail.com>
2025-03-30 00:02:04 -0700

    Update Data-visualization-app-deployment.R
    Please don't steal my shiny app info when I forgot to take it out before

--- Data-visualization-app-deployment.R
+++ Data-visualization-app-deployment.R
@@ -4,11 +4,11 @@ library(rsconnect)
 # Step 2 and 3: Obtain Account Information and Add Your Account to rsconnect
 rsconnect::setAccountInfo(
   name='keni-17', 
-  token='C1B0A00830B4DAEF7074432A817D23C2', 
-  secret='75HamsLrxEH4wwkEqxpW9vnrRlzsfmVk+Kbtb0Ia'
+  token='****', 
+  secret='****'
 )
 
 # Step 4 and 5: Prepare your Shiny App and Deploy your App
-rsconnect::deployApp("C:/Users/kenic/OneDrive/Documents/4/STAT321/Labs/Lab 4")
+rsconnect::deployApp("C:/Users/kenic/OneDrive/Documents/GitHub/stat321-project/nflapp")
 
-# url: <https://keni-17.shinyapps.io/lab_4/>
+# url: <https://keni-17.shinyapps.io/nflapp/>
